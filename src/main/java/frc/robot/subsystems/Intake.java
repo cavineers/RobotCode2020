4884ae524bafc160d2a517117a8d451403f7c126
Bryce Cary <bcary.md@gmail.com>
2020-01-08 14:30:18 -0500

    Created 'Intake' subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,16 @@
+// TODO: Everything
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  public Intake() {
+
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+}
