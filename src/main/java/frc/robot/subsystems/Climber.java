5ea2467e43766fec8cf904543b05b02e71de6a87
Bryce Cary <bcary.md@gmail.com>
2020-01-08 14:29:59 -0500

    Created 'Climber' subsystem

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,16 @@
+// TODO: Everything
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+  public Climber() {
+
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+}
