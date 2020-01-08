8797e674c05c62136ee227bdf75d1a3a294dbe0c
Bryce Cary <bcary.md@gmail.com>
2020-01-08 14:28:04 -0500

    Quick clean up

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -1,9 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+//! UNTESTED
 
 package frc.robot.subsystems;
 
@@ -38,6 +33,6 @@ public class ColorSensor extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
 
-    // TODO poll color sensor and save data
+    // TODO: poll color sensor and save data
   }
 }
