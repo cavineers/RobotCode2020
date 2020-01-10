0be4502483baa47d60dc3be33375ef647b973f58
Andrew <phelpsag05@gmail.com>
2020-01-10 05:57:42 -0500

    Added motor to subsystem and Intake to robot

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,10 +2,27 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PWM; 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import com.ctre.phoenix.sensors.CANCoder; 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.CANifier;
+
 
 public class Intake extends SubsystemBase {
+  public TalonSRX ballIntakeMotor = new TalonSRX(Constants.kIntakeMotorCANid);
+private boolean isOn= false; 
+public float intakePosition=0;
   public Intake() {
+this.getIntakeMotor().setNeutralMode(NeutralMode.Brake);
 
   }
 
@@ -13,4 +30,17 @@ public class Intake extends SubsystemBase {
   public void periodic() {
     
   }
+ 
+
+    /**
+     * get the talon of elevator motor
+     * @return talon of elevator motor
+     */
+    public TalonSRX getIntakeMotor() {
+        return this.ballIntakeMotor;
+    }
+   public void getTargetPosition(){
+   
+   }
+    
 }
