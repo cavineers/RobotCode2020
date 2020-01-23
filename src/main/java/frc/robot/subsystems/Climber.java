ef25c6ca45c4f0fcfff4bd78ec06e63e7b56ada3
Caroline Keller <kellerca22@spaldinghs.org>
2020-01-23 15:25:51 -0500

    Added the motor for the winch and methods

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,12 +2,35 @@
 
 package frc.robot.subsystems;
 
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 
 public class Climber extends SubsystemBase {
-  public Climber() {
 
+  CANSparkMax winchMotor = new CANSparkMax(Constants.kClimberMotorCANid, MotorType.kBrushless);
+  private boolean brakeMode;
+
+  public Climber() {
+    this.getWinchMotor();
+    this.setBrakeMode(true);
   }
+
+
+  public CANSparkMax getWinchMotor() {
+    return winchMotor;
+  }
+
+  private void setBrakeMode(boolean on) {
+    if (brakeMode != on) {
+      brakeMode = on;
+    }
+  }
+
 
   @Override
   public void periodic() {
