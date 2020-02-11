f102004b81b8b472ba717fc97a632c0210839f8a
Bryce Cary <bcary.md@gmail.com>
2020-02-10 22:28:08 -0500

    Added gyro

---
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Gyro extends SubsystemBase {
+    // Create gyro
+    private AHRS navx = new AHRS(SerialPort.Port.kMXP);
+
+    public Gyro() {} // nothing...
+
+    @Override
+    public void periodic() {} // and more nothing...
+
+    // Expose gyro
+    public AHRS getGyro() {
+        return this.navx;
+    }
+}
